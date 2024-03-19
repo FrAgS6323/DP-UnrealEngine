d0e8e6a4e761d800b1604e68fdd71186721ae878
FrAgS6323 <108538591+FrAgS6323@users.noreply.github.com>
2024-03-19 22:32:55 +0100

    Imported Tube models
    Imported Tube models.

---
+++ Source/TubeUE/TubeUEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TubeUEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUBEUE_API ATubeUEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
